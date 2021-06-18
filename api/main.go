ee32dc392febbcc61cda09ad28148a387084306c
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-06-17 19:53:37 -0500

    Primer commit

--- api/main.go
+++ api/main.go
@@ -2,10 +2,28 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	url := "https://www.google.com/search?q=usd+to+cop&oq=usd+to+cop&aqs=chrome..69i57.1749j0j4&sourceid=chrome&ie=UTF-8"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3776.0 Safari/537.36")
+	client := &http.Client{Timeout: time.Second * 5}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil && data == nil {
+		fmt.Println(err)
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("Hola mundo")
+	json.NewEncoder(w).Encode(result)
 }
